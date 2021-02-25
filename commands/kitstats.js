7dc23dbaf562f0a04ad65f17cad96897f3dfc559
MasterTemple <dgmastertemple@protonmail.com>
2021-02-24 22:51:09 -0800

    kit stats do piece bonus

--- commands/kitstats.js
+++ commands/kitstats.js
@@ -5,7 +5,7 @@ module.exports = {
     use: `kitstats [name]`,
     example:[`kitstats daredevil rank 3 variant`],
     execute(message, args) {
-        console.log(`me`)
+        //console.log(`me`)
         function err(){
             const func = require(`./embed.js`);
             try {
@@ -41,7 +41,7 @@ module.exports = {
         //if(isNaN(parseInt(args[0]))==false) {
         var map_id = args[0]
         map_id = map_id.toLowerCase()
-        console.log(map_id)
+        //console.log(map_id)
         //var folder_loc = Math.floor(id / 256)
         try {
             const client = message.client
@@ -50,6 +50,11 @@ module.exports = {
 
             //var data = item.Sets.find(a => (a.itemIDs.includes(args[0])))
             var data = item.Sets.find(a => (a.setName.toLowerCase().includes(search)))
+            //var test = require(`./../search/itemSets.json/Sets/55/piecebonus3/armor`);
+
+
+            //console.log(data.piecebonus4["armor"])
+
             //console.log(data)
 
             //var data = item.Sets.some((val) => {
@@ -76,7 +81,7 @@ module.exports = {
 
                 var folder_loc = Math.floor(data.itemIDs[i] / 256)
                 var stat = require(`./../objects/0/${folder_loc}/${data.itemIDs[i]}.json`);
-                console.log(`./../objects/0/${folder_loc}/${data.itemIDs[i]}.json`)
+                //console.log(`./../objects/0/${folder_loc}/${data.itemIDs[i]}.json`)
                 if (stat.skills != undefined) {
                     //console.log(stat.skills)
                     var Armor = 0
@@ -89,7 +94,7 @@ module.exports = {
                         //var behav_folder_loc = Math.floor(skillID / 256)
                         //var skillBehavior = require(`./../locale/SkillBehavior/${behav_folder_loc}.json`)
                         var cooldownFile = require(`./../tables/SkillBehavior/${skillID}.json`)
-                        console.log(cooldownFile)
+                        //console.log(cooldownFile)
                         //console.log(data.enemies[i])
 
                         if (cooldownFile.armorBonusUI != null) {
@@ -115,10 +120,46 @@ module.exports = {
 
 
                     let lootdata = loot.Sheet1.find(a => (a.id) == data.itemIDs[i])
-                    pieces = `${pieces}${data.itemIDs[i]}: **${lootdata.name}**\nArmor: **${Armor}** Health: **${Health}** Imagination: **${Imagination}**\n`
+                    pieces = `${pieces}${data.itemIDs[i]}: **${lootdata.name}**\n**+**Armor: **${Armor}** Health: **${Health}** Imagination: **${Imagination}**\n`
 
                 }
 
+            }
+            //data.piecebonus
+            if(data.piecebonus2!=undefined){
+                console.log(data.piecebonus2)
+                totalArmor += parseInt(data.piecebonus2["armor"])
+                totalHealth += parseInt(data.piecebonus2["health"])
+                totalImagination += parseInt(data.piecebonus2["imagination"])
+                pieces = `${pieces}**2 Piece Bonus**\n**+**Armor: **${data.piecebonus2["armor"]}** Health: **${data.piecebonus2["health"]}** Imagination: **${data.piecebonus2["imagination"]}**\n`
+            }
+            if(data.piecebonus3!=undefined){
+                console.log(data.piecebonus3)
+                totalArmor += parseInt(data.piecebonus3["armor"])
+                totalHealth += parseInt(data.piecebonus3["health"])
+                totalImagination += parseInt(data.piecebonus3["imagination"])
+                pieces = `${pieces}**3 Piece Bonus**\n**+**Armor: **${data.piecebonus3["armor"]}** Health: **${data.piecebonus3["health"]}** Imagination: **${data.piecebonus3["imagination"]}**\n`
+            }
+            if(data.piecebonus4!=undefined){
+                console.log(data.piecebonus4)
+                totalArmor += parseInt(data.piecebonus4["armor"])
+                totalHealth += parseInt(data.piecebonus4["health"])
+                totalImagination += parseInt(data.piecebonus4["imagination"])
+                pieces = `${pieces}**4 Piece Bonus**\n**+**Armor: **${data.piecebonus4["armor"]}** Health: **${data.piecebonus4["health"]}** Imagination: **${data.piecebonus4["imagination"]}**\n`
+            }
+            if(data.piecebonus5!=undefined){
+                console.log(data.piecebonus5)
+                totalArmor += parseInt(data.piecebonus5["armor"])
+                totalHealth += parseInt(data.piecebonus5["health"])
+                totalImagination += parseInt(data.piecebonus5["imagination"])
+                pieces = `${pieces}**5 Piece Bonus**\n**+**Armor: **${data.piecebonus5["armor"]}** Health: **${data.piecebonus5["health"]}** Imagination: **${data.piecebonus5["imagination"]}**\n`
+            }
+            if(data.piecebonus6!=undefined){
+                console.log(data.piecebonus6)
+                totalArmor += parseInt(data.piecebonus6["armor"])
+                totalHealth += parseInt(data.piecebonus6["health"])
+                totalImagination += parseInt(data.piecebonus6["imagination"])
+                pieces = `${pieces}**6 Piece Bonus**\n**+**Armor: **${data.piecebonus6["armor"]}** Health: **${data.piecebonus6["health"]}** Imagination: **${data.piecebonus6["imagination"]}**\n`
             }
             pieces = `${pieces}\n**Total**\nArmor: **${totalArmor}** Health: **${totalHealth}** Imagination: **${totalImagination}**\n`
 
