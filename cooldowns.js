e1c49bf270ae2a5e6bbb17d9e330d75ce4140790
MasterTemple <dgmastertemple@protonmail.com>
2021-02-25 01:04:29 -0800

    log

--- cooldowns.js
+++ cooldowns.js
@@ -2,4 +2,19 @@ const cool = require(`./cooldown4.json`)
 
 
 //console.log(cool["Cooldowns"])
-console.log(cool["Cooldowns"][0][96][0])
+console.log(Object.keys(cool["Cooldowns"]).length)
+console.log(Object.keys(cool["Cooldowns"][0]).length)
+console.log(Object.keys(cool["Cooldowns"][0][0]).length)
+//console.log(cool["Cooldowns"][0].length)
+console.log(cool["Cooldowns"][0][96].length)
+var desc = ``
+for(var i=0;i<=103;i++){
+    desc =`${desc}\nCooldownGroup:${i}\n`
+    //console.log(cool["Cooldowns"][0][i])
+    for(var j=0;j<cool["Cooldowns"][0][i].length;j++){
+        desc =`${desc}skillID:${cool["Cooldowns"][0][i][j]} `
+
+    }
+}
+
+console.log(desc)
